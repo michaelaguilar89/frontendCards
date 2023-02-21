4476ecf7d7c0c533d0657440ca08e641c996a4b9
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-21 12:59:41 -0600

    config app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUpdateCardComponent } from './components/create-update-card/create-update-card.component';
+import { ListCardComponent } from './components/list-card/list-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path':'list',component:ListCardComponent},
+  {'path':'create',component:CreateUpdateCardComponent},
+  {'path':'update',component:CreateUpdateCardComponent},
+  {'path':'',redirectTo:'list',pathMatch:'full'},
+  {'path':'**',redirectTo:'list',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

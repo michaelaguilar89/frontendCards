e9445d369658e3a760ef25eb1b6e5edba40e4485
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-22 10:48:32 -0600

    adding getCards on card-service

--- src/app/services/card-service.service.ts
+++ src/app/services/card-service.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Card } from '../models/card';
 @Injectable({
   providedIn: 'root'
 })
 export class CardServiceService {
+ private url='https://localhost:5001/api/Cards/';
+ list:any;
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+
+  getCards(){
+    this.http.get(this.url).toPromise().then(
+      data=>{
+        this.list=data;  
+      }
+    )
+  }
+
 }
+

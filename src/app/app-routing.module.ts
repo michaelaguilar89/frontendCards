726b46c121a0af6a3da77f6299b70d959c647596
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-03-01 00:34:34 -0600

    cnfiguring app.routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUpdateCardComponent } from './components/create-update-card/create-update-card.component';
+import { GetByIdRemoveCardComponent } from './components/get-by-id-remove-card/get-by-id-remove-card.component';
 import { ListCardComponent } from './components/list-card/list-card.component';
 
 const routes: Routes = [
   {'path':'list',component:ListCardComponent},
   {'path':'create',component:CreateUpdateCardComponent},
   {'path':'update',component:CreateUpdateCardComponent},
+  {'path':'getById',component:GetByIdRemoveCardComponent},
+  {'path':'remove',component:GetByIdRemoveCardComponent},
   {'path':'',redirectTo:'list',pathMatch:'full'},
   {'path':'**',redirectTo:'list',pathMatch:'full'}
 ];
